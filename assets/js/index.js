081833e7365873b9368d3293f8ef8248f6303062
tifukelison <tifukelison@gmail.com>
2022-10-11 11:31:10 +0100

    Add files via upload

---
+++ assets/js/index.js
@@ -0,0 +1,31 @@
+  // Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyBmZGGakr5OpUiYAKbuR7dMOYlUloagUX8",
+
+    authDomain: "kekepf-28364.firebaseapp.com",
+
+    projectId: "kekepf-28364",
+
+    storageBucket: "kekepf-28364.appspot.com",
+
+    messagingSenderId: "653393635823",
+
+    appId: "1:653393635823:web:f27162cd40a15d1b5d2e1a"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
